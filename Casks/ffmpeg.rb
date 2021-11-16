d1ac92b3631eeeecb0680abf59e641706c264aca
Brandon Siegel <bsiegel@users.noreply.github.com>
2021-11-16 21:36:20 +0000

    Update ffmpeg from 4.4 to 4.4.1

--- Casks/ffmpeg.rb
+++ Casks/ffmpeg.rb
@@ -1,18 +1,18 @@
 cask "ffmpeg" do
-  version "4.4"
-  sha256 "ee7fa287e35bb964fd7a1dfacc388b96cf376f090d4cbbb9d75d707e313b9e98"
+  version "4.4.1"
+  sha256 "d7f3ea44b6086834fac4198f2f0a82f93ac9fba587d94f6b086b16ed228fcdb3"
 
   url "https://evermeet.cx/ffmpeg/ffmpeg-#{version}.zip"
   name "FFmpeg"
   desc "Play, record, convert, and stream audio and video"
   homepage "https://evermeet.cx/ffmpeg/"
 
+  livecheck do
+    url "https://evermeet.cx/ffmpeg/info/ffmpeg/release"
+    regex(/\A{"name":"ffmpeg","type":"release","version":"(\d+(?:\.\d+)*)"/)
+  end
+
   conflicts_with formula: "ffmpeg"
 
   binary "ffmpeg"
-
-  livecheck do
-    url "https://evermeet.cx/ffmpeg/info/ffmpeg/release"
-    regex(%r[\A{"name":"ffmpeg","type":"release","version":"(\d+(?:\.\d+)*)"])
-  end
 end

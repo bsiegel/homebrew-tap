966bc20d3a5d3688fc25c60113da077bc10f0c67
Brandon Siegel <96068+bsiegel@users.noreply.github.com>
2021-11-16 13:38:51 -0800

    Update ffprobe from 4.4 to 4.4.1 (#7)
    Co-authored-by: Brandon Siegel <bsiegel@users.noreply.github.com>

--- Casks/ffprobe.rb
+++ Casks/ffprobe.rb
@@ -1,18 +1,18 @@
 cask "ffprobe" do
-  version "4.4"
-  sha256 "f574171f90686747ef12504fb4f8629ce3f9493c13727f928d594d7ac0d66169"
+  version "4.4.1"
+  sha256 "c0b506884064dec8cde87d0f0c447a2294d615f518cbde337d3345e6058e0160"
 
   url "https://evermeet.cx/ffmpeg/ffprobe-#{version}.zip"
   name "FFprobe"
   desc "Play, record, convert, and stream audio and video"
   homepage "https://evermeet.cx/ffmpeg/"
 
+  livecheck do
+    url "https://evermeet.cx/ffmpeg/info/ffprobe/release"
+    regex(/\A{"name":"ffprobe","type":"release","version":"(\d+(?:\.\d+)*)"/)
+  end
+
   conflicts_with formula: "ffmpeg"
 
   binary "ffprobe"
-
-  livecheck do
-    url "https://evermeet.cx/ffmpeg/info/ffprobe/release"
-    regex(%r[\A{"name":"ffprobe","type":"release","version":"(\d+(?:\.\d+)*)"])
-  end
 end

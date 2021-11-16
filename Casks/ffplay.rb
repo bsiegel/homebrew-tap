bc6e68926fcf04d22619e0d13626d9f10d80c876
Brandon Siegel <96068+bsiegel@users.noreply.github.com>
2021-11-16 13:38:38 -0800

    Update ffplay from 4.4 to 4.4.1 (#6)

--- Casks/ffplay.rb
+++ Casks/ffplay.rb
@@ -1,18 +1,18 @@
 cask "ffplay" do
-  version "4.4"
-  sha256 "ccc90fcfd90d3be527f0a1a589b1802cfaebf7d112a407e2a6f14856b05d4878"
+  version "4.4.1"
+  sha256 "4d170df1e87537dee96365f58b2b207cac5401ec0808b4808fac60e870999c9e"
 
   url "https://evermeet.cx/ffmpeg/ffplay-#{version}.zip"
   name "FFplay"
   desc "Play, record, convert, and stream audio and video"
   homepage "https://evermeet.cx/ffmpeg/"
 
+  livecheck do
+    url "https://evermeet.cx/ffmpeg/info/ffplay/release"
+    regex(/\A{"name":"ffplay","type":"release","version":"(\d+(?:\.\d+)*)"/)
+  end
+
   conflicts_with formula: "ffmpeg"
 
   binary "ffplay"
-
-  livecheck do
-    url "https://evermeet.cx/ffmpeg/info/ffplay/release"
-    regex(%r[\A{"name":"ffplay","type":"release","version":"(\d+(?:\.\d+)*)"])
-  end
 end

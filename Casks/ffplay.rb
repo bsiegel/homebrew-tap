c9a435c6f96ed3a644e750e4c5991219dac01867
Brandon Siegel <96068+bsiegel@users.noreply.github.com>
2022-04-17 12:17:07 -0700

    Update ffplay from 5.0 to 5.0.1 (#12)
    Co-authored-by: Brandon Siegel <bsiegel@users.noreply.github.com>

--- Casks/ffplay.rb
+++ Casks/ffplay.rb
@@ -1,6 +1,6 @@
 cask "ffplay" do
-  version "5.0"
-  sha256 "a6b17e9f6d200528b0fb021d097fa49cdba051a75ca8b6b1eefe21a548297611"
+  version "5.0.1"
+  sha256 "d172e6ad38eccb730d3a818698ff85a8bf3b13e2e3ff680af8fda898ff7b1fe0"
 
   url "https://evermeet.cx/ffmpeg/ffplay-#{version}.zip"
   name "FFplay"

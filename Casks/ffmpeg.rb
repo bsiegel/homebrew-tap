a59c03dc8bac289b0c6c939ebf3ec6dbc794d074
Brandon Siegel <96068+bsiegel@users.noreply.github.com>
2024-04-05 12:22:33 -0700

    ffmpeg 7.0 (#17)

--- Casks/ffmpeg.rb
+++ Casks/ffmpeg.rb
@@ -1,33 +1,33 @@
 cask "ffmpeg" do
-  arch arm: "arm",
-     intel: "intel"
+  arch arm:   "arm",
+       intel: "intel"
 
   on_arm do
-    version "6.1.1"
-    sha256 "1c69a9a4a5d0172d9aec611f19d282b38b3c3d69832621ecef87926335f62e0a"
+    version "7.0"
+    sha256 "d2cd3145597a36ec17cb55273f1b7c2ce80def3b67e915293e649cd66281160c"
   end
   on_intel do
-    version "6.1"
-    sha256 "9a7de6ea24052166340c50e00dd493da49315431e6bfde5ed2a600eab9f27b65"
+    version "7.0"
+    sha256 "2d01a9bb00c3d0d4a850baa12a9414af197c1199315443bce44064ffb8e4297a"
   end
 
   url_version = [
     version.major.to_i,
-    ((version.minor.to_i || 0) if (version.minor.to_i || 0) > 0 || (version.patch.to_i || 0) > 0),
-    ((version.patch.to_i || 0) if (version.patch.to_i || 0) > 0)
-  ].compact.join("")
+    (version.minor.to_i || 0 if (version.minor.to_i || 0).positive? || (version.patch.to_i || 0).positive?),
+    (version.patch.to_i || 0 if (version.patch.to_i || 0).positive?),
+  ].compact.join
 
   url "https://www.osxexperts.net/ffmpeg#{url_version}#{arch}.zip"
   name "FFmpeg"
   desc "Play, record, convert, and stream audio and video"
   homepage "https://www.osxexperts.net/"
 
-  conflicts_with formula: "ffmpeg"
-
-  binary "ffmpeg"
-
   livecheck do
     url "https://www.osxexperts.net/"
     regex(/Download ffmpeg (\d+(?:\.\d+)+)/i)
   end
+
+  conflicts_with formula: "ffmpeg"
+
+  binary "ffmpeg"
 end

aa703914737587de8f44a4a1477dd42b08742f2d
Brandon Siegel <96068+bsiegel@users.noreply.github.com>
2024-04-05 12:32:13 -0700

    ffplay 7.0 (#18)
    Co-authored-by: Brandon Siegel <bsiegel@users.noreply.github.com>

--- Casks/ffplay.rb
+++ Casks/ffplay.rb
@@ -1,33 +1,33 @@
 cask "ffplay" do
-  arch arm: "arm",
-     intel: "intel"
+  arch arm:   "arm",
+       intel: "intel"
 
   on_arm do
-    version "6.1.1"
-    sha256 "863136f1e1c1e3065851ffb37f0d8480959f955ff08adc179ff8b306cf9a8b26"
+    version "7.0"
+    sha256 "b3803b97afd30028aa5bba1cf88fbb3cde935224f317c2aa8879af77aba2eee1"
   end
   on_intel do
-    version "6.1"
-    sha256 "ce4829530ccca6d8472bb1aa97af04e3f7a494bc815b303a404256d0190ab8e4"
+    version "7.0"
+    sha256 "d018dd609980b62fd4e5e60c0f8e8853681fb3e6971c6dd25150088b57cd3a37"
   end
 
   url_version = [
     version.major.to_i,
-    ((version.minor.to_i || 0) if (version.minor.to_i || 0) > 0 || (version.patch.to_i || 0) > 0),
-    ((version.patch.to_i || 0) if (version.patch.to_i || 0) > 0)
-  ].compact.join("")
+    (version.minor.to_i || 0 if (version.minor.to_i || 0).positive? || (version.patch.to_i || 0).positive?),
+    (version.patch.to_i || 0 if (version.patch.to_i || 0).positive?),
+  ].compact.join
 
   url "https://www.osxexperts.net/ffplay#{url_version}#{arch}.zip"
   name "FFmpeg"
   desc "Play, record, convert, and stream audio and video"
   homepage "https://www.osxexperts.net/"
 
-  conflicts_with formula: "ffmpeg"
-
-  binary "ffplay"
-
   livecheck do
     url "https://www.osxexperts.net/"
     regex(/Download ffplay (\d+(?:\.\d+)+)/i)
   end
+
+  conflicts_with formula: "ffmpeg"
+
+  binary "ffplay"
 end

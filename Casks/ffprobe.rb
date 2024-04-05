0a73fc8d74580b2f241e22d90eb8a9aedf933484
Brandon Siegel <bsiegel@users.noreply.github.com>
2024-04-05 19:31:39 +0000

    ffprobe 7.0

--- Casks/ffprobe.rb
+++ Casks/ffprobe.rb
@@ -1,33 +1,33 @@
 cask "ffprobe" do
-  arch arm: "arm",
-     intel: "intel"
+  arch arm:   "arm",
+       intel: "intel"
 
   on_arm do
-    version "6.1.1"
-    sha256 "8ec78ce09c0b474ae7583908c8d79d801f754d8273dca3d5a5de51f307517fc7"
+    version "7.0"
+    sha256 "bf0cccdda082ad7ed0d04cc6767014bce86f0a83bcd4a7b85137667ed0bcee0e"
   end
   on_intel do
-    version "6.1"
-    sha256 "977381d8cec9c477fef4cdbb259addc7e5506e47fa61087e620d5ed01a7c7a51"
+    version "7.0"
+    sha256 "e14a2feb619ee21cb96ac3bf8c34a5d57f2ea61c38fe9d89b1a084d619b6aebf"
   end
 
   url_version = [
     version.major.to_i,
-    ((version.minor.to_i || 0) if (version.minor.to_i || 0) > 0 || (version.patch.to_i || 0) > 0),
-    ((version.patch.to_i || 0) if (version.patch.to_i || 0) > 0)
-  ].compact.join("")
+    (version.minor.to_i || 0 if (version.minor.to_i || 0).positive? || (version.patch.to_i || 0).positive?),
+    (version.patch.to_i || 0 if (version.patch.to_i || 0).positive?),
+  ].compact.join
 
   url "https://www.osxexperts.net/ffprobe#{url_version}#{arch}.zip"
   name "FFmpeg"
   desc "Play, record, convert, and stream audio and video"
   homepage "https://www.osxexperts.net/"
 
-  conflicts_with formula: "ffmpeg"
-
-  binary "ffprobe"
-
   livecheck do
     url "https://www.osxexperts.net/"
     regex(/Download ffprobe (\d+(?:\.\d+)+)/i)
   end
+
+  conflicts_with formula: "ffmpeg"
+
+  binary "ffprobe"
 end
